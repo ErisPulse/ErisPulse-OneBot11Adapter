09977e731c2326b09c881c55ab91e41b0df0bfc2
Auto <auto@builder.com>
2025-06-07 01:19:06 +0800

    build: 更新 OneBotAdapter 模块版本

--- OneBotAdapter/__init__.py
+++ OneBotAdapter/__init__.py
@@ -1,7 +1,7 @@
 moduleInfo = {
     "meta": {
         "name": "OneBotAdapter",
-        "version": "2.0.0",
+        "version": "2.0.1",
         "description": "OneBotV11协议适配模块，异步的OneBot触发器",
         "author": "WSu2059",
         "license": "MIT",
