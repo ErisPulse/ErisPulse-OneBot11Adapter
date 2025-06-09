92b81b54f2170348f6baf20f3d5a2d5572067c05
wsu2059q <wsu2059@qq.com>
2025-06-10 00:12:05 +0800

    feat(OneBotAdapter): 增加对大小写不敏感的适配器识别

--- OneBotAdapter/__init__.py
+++ OneBotAdapter/__init__.py
@@ -1,7 +1,7 @@
 moduleInfo = {
     "meta": {
         "name": "OneBotAdapter",
-        "version": "2.0.8",
+        "version": "2.0.9",
         "description": "OneBotV11协议适配模块，异步的OneBot触发器",
         "author": "WSu2059",
         "license": "MIT",
@@ -18,7 +18,11 @@ from .Core import Main
 from .Core import OneBotAdapter
 
 adapterInfo = {
-    "qq": OneBotAdapter
+    "qq": OneBotAdapter,
+    "QQ": OneBotAdapter,
+    "onebot": OneBotAdapter
 }
 
 # build_hash="f0ccac80fe3e29e0ca054f0c683b36266f1224dffc2f8fcd5a70f19b6ec0707f"
+
+# build_hash="5ec0fa239c5fad571e8e039d8556e59ee243ec982f0661580fb4caa7774ee3e2"
