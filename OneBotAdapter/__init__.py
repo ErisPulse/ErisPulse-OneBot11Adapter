43c43da7c2ddbc5c99eb787e65db044f10afc6b5
wsu2059q <wsu2059@qq.com>
2025-06-14 23:20:25 +0800

    build(OneBotAdapter): 修改注册信息；更新模块版本至 2.1.1

--- OneBotAdapter/__init__.py
+++ OneBotAdapter/__init__.py
@@ -1,7 +1,7 @@
 moduleInfo = {
     "meta": {
         "name": "OneBotAdapter",
-        "version": "2.1.0",
+        "version": "2.1.1",
         "description": "OneBotV11协议适配模块，异步的OneBot触发器",
         "author": "WSu2059",
         "license": "MIT",
@@ -19,9 +19,8 @@ from .Core import OneBotAdapter
 
 adapterInfo = {
     "qq": OneBotAdapter,
-    "QQ": OneBotAdapter,
     "onebot": OneBotAdapter
 }
 
 
-# build_hash="cb8a57cd1c6754a634bb0783f77757f8ad1b2da29b4d9b6467fee2756ef6faa9"
+# build_hash="fcb24dd06cd5386dbf27416f20b1f4397d7fa1b910b4346b841a1b42f7b002c6"
