55514d4f36801400d05496ca0b51726a5c56a647
wsu2059q <wsu2059@qq.com>
2025-06-21 07:08:30 +0800

    fix(OneBotAdapter): 修改 Send 类的继承关系并更新版本号至 2.2.0

--- OneBotAdapter/__init__.py
+++ OneBotAdapter/__init__.py
@@ -1,7 +1,7 @@
 moduleInfo = {
     "meta": {
         "name": "OneBotAdapter",
-        "version": "2.1.1",
+        "version": "2.2.0",
         "description": "OneBotV11协议适配模块，异步的OneBot触发器",
         "author": "WSu2059",
         "license": "MIT",
@@ -24,3 +24,5 @@ adapterInfo = {
 
 
 # build_hash="fcb24dd06cd5386dbf27416f20b1f4397d7fa1b910b4346b841a1b42f7b002c6"
+
+# build_hash="4d10d7c31005bb7dc546a08b635effbcb73faa8885d6fd289b71293229c1933b"
